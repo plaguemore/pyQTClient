9a3da73f743d1baed4c9f139a61ddb7c980a7b3d
onfroy x <qwqlol.7696@gmail.com>
2022-10-24 15:31:38 +0300

    Send Message

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
 
 from PyQt6 import uic, QtCore, QtGui, QtWidgets
+import datetime
+import requests
+from requests.exceptions import HTTPError
+import json
 
 class MainWindow(QtWidgets.QMainWindow):
     ServerAdress = "http://localhost:5000"
@@ -9,6 +13,22 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi('messenger.ui', self)
+        self.pushButton1.clicked.connect(self.pushButton1_clicked)
+
+    def pushButton1_clicked(self):
+        self.SendMessage()
+
+    def SendMessage(self):
+        UserName = self.lineEdit1.text()
+        MessageText = self.lineEdit2.text()
+        TimeStamp = str(datetime.datetime.today())
+        msg = f"{{\"UserName\": \"{UserName}\", \"MessageText\": \"{MessageText}\", \"TimeStamp\": \"{TimeStamp}\"}}"
+        # {"UserName": "RusAl", "MessageText": "Privet na sto let!!!", "TimeStamp": "2021-03-05T18:23:10.932973Z"}
+        print("Отправлено сообщение: " + msg)
+        url = self.ServerAdress + "/api/Messanger"
+        data = json.loads(msg)  # string to json
+        r = requests.post(url, json=data)
+        # print(r.status_code, r.reason)
 
 
 
